624b41bbd750dad3584029b02ff1c95245ff7394
Liam Ronan <liam@digit.co>
2019-06-13 13:08:13 -0700

    update apple simutils to ref digit master branch

--- applesimutils.rb
+++ applesimutils.rb
@@ -1,7 +1,7 @@
 class Applesimutils < Formula
   desc 'Apple simulator utilities'
   homepage 'https://github.com/wix/AppleSimulatorUtils'
-  url 'https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.6.4.tar.gz'
+  url 'https://github.com/hellodigit/homebrew-brew/blob/master/AppleSimulatorUtils-0.6.4.tar.gz'
   sha256 '7b32c84b4284b459dbe74a3076b7f8694b9f18dea023a2d99e2b32c6e67edd00'
   head 'https://github.com/wix/AppleSimulatorUtils.git'
 
